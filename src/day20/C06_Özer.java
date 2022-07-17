a40de00fb8a6d5fb712fa3ae1158f01a00fe93c5
Fatma Esma Tüzen <93475138+esmatuzen@users.noreply.github.com>
2022-07-17 18:17:13 +0300

    Add files via upload

---
+++ src/day20/C06_Özer.java
@@ -0,0 +1,32 @@
+package day20;
+
+public class C06_Özer {
+    /*
+    bir variableslın array oldugu belli etmek için [] kullanılır
+    int sayi1=10; sadece 1 deger store edebilir
+
+    int sayi2[]= { 5,6,7,8,10}; ınt degerler barındıran bir array de olabilir .non prımitiv data türlerınden
+    farklı olarak bir den fazla deger atanabilir
+
+    bir array olustururken barındıracagı data turu ve kac data bulunduracagı belirtilmelidir.
+    int ıse int oldugu mutlaka yazılmalı
+    char yada blooen gibi
+    içine kac tane eleman koyacagınızıda belirtmelisiniz buna da lengt (uzunlugu)deniyor.{} içindekiler
+
+   degerleri yanı {} içine yazmazsak bile kac tabe data koyacagımızı belirtmemiz gerekecektir.
+
+   int sayi4[] = new int [6] ınt data  turunde  6 data barındırır.
+   degerleri bastan eklemezsek java istedigimiz uzunlukta bir array olusturacak ve tum elemanlara default degerler atayacaktır
+   int sayısal degerler ıcın 0
+   boolean icin false
+   char "
+   String null
+   array olusturulduktan sonra arrayın uzunlugu değiştrilmez.
+   !!!! onemlii
+   bir array olusturduktan sonra var olan datalar uptate eklemek veya varolan datalardan birini tamamen silmek yanı arryın
+   uzunluğunu etileyeck durumlar soz konusu degildir .
+   ** bir arrayinb eleman
+
+
+     */
+}
